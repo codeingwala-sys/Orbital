3951162258cac8cd86da79641fb4878dd599e045
Shibalik Pal <codeingwala@gmail.com>
2026-02-14 13:54:32 +0530

    Updated Server
    Server can now run on both hosted frontend and local host

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const PORT = process.env.PORT || 3000;
 const USERS_FILE = path.resolve("./users.json");
 
 app.use(cors({
-  origin: "https://codeingwala-sys.github.io",
+  origin: [
+    "https://codeingwala-sys.github.io",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://localhost:3000"
+  ],
   methods: ["GET", "POST", "PUT"],
   allowedHeaders: ["Content-Type", "x-dev-secret"]
 }));
+
 
 app.use(express.json());
 
