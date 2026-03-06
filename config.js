5987f8b98c5ac4ecbd4019f7afd7c825d279ae42
Shibalik Pal <codeingwala@gmail.com>
2026-03-06 18:46:58 +0530

    V_3.7 Frontend

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-// config.js
-window.BACKEND_URL = "https://orbital-2.onrender.com";
+config.js
+window.BACKEND_URL = "https://orbital-3.onrender.com";
+// window.BACKEND_URL = "http://localhost:3000";
